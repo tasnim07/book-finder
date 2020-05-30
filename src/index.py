a11790e1c742d94d36480b2c3cab5bba81d8bf86
Tasneem Haider <tasnim07@gmail.com>
2020-05-30 16:07:56 +0530

    added project skeleton with required files and directory

---
+++ src/index.py
@@ -0,0 +1,18 @@
+def index():
+    pass
+
+
+def remove_punctuations():
+    pass
+
+
+def process_text():
+    pass
+
+
+def generate_tokens():
+    pass
+
+
+def remove_stopwords():
+    pass
---
+++ src/search.py
@@ -0,0 +1,17 @@
+def search():
+    pass
+
+
+def score():
+    pass
+
+
+def get_term_frequency():
+    pass
+
+
+def get_idf():
+    pass
+
+
+# we can use decorator pattern to calculate score
